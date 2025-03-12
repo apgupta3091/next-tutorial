861ff994072ec2ab43748d68f8d36af60fa4e7ea
Anuj Gupta <anuj@cyberdive.co>
2025-03-12 15:58:13 -0400

    learning next

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,23 @@
+import Navbar from '@/components/Navbar';
 import './globals.css';
+import { Inter } from "next/font/google";
+import type { Metadata } from 'next';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Next.js Project',
+  description: 'A Next.js project with TypeScript and TailwindCSS.',
+  keywords: 'Next.js, Typescript, TailwindCSS',
+};
 
 export default function RootLayout({ children } : Readonly<{children: React.ReactNode }>) {
   return <html lang="en">
-    <body>
-      <nav>Hello There</nav>
-      {children}
+    <body className={inter.className}>
+      <Navbar />
+      <main className='max-w-3xl mx-auto py-10'>
+        {children}
+      </main>
     </body>
   </html>
 }
